00b1e2dd1882f8ca7fa494e6661e659e27337c2f
Justin Donato <justin.donato@gmail.com>
2014-10-08 21:23:50 -0400

    move app into its own file, adds login form and renders photos

---
+++ examples/parse-app/app.js
@@ -0,0 +1,93 @@
+(function() {
+
+    Parse.initialize("anBpWcFi4tt8WN5nIFJ2T2eaRLp9rCGhxOWZohtQ", "0bKnqYCRZhCr0OorUipeh5KoFbvvWm5zpYzOkVW8");
+
+    var Photo = Parse.Object.extend("Photo");
+
+    window.navigateTo = function (page) {
+        window.location = page;
+    }
+
+    var renderPhoto = function (photo) {
+        var li = $('<li/>');
+
+        var img = $('<img/>');
+        img.attr('src', photo.get('photo').url());
+
+        var caption = $('<p/>');
+        caption.text(photo.get('caption'));
+
+        li.append(img);
+        li.append(caption);
+
+        return li;
+    }
+
+    var renderPhotos = function (photos) {
+        var photosList = $('#list');
+
+        for (var i = 0; i < photos.length; i++) {
+            photosList.append(renderPhoto(photos[i]));
+        }
+
+    }
+
+    var refreshPhotos = function() {
+
+        var query = new Parse.Query(Photo);
+        query.equalTo('user', currentUser);
+
+        query.find({
+            success: function(results) {
+                renderPhotos(results);
+            }
+        });
+
+    }
+
+    var handleUploadSubmit = function(e) {
+
+        e.preventDefault();
+
+        var fileUploadControl = $("#photo").get(0);
+
+        if (fileUploadControl.files.length > 0) {
+
+            var file = fileUploadControl.files[0];
+            var name = "photo.jpg";
+
+            var photo = new Photo();
+
+            photo.set('user', currentUser);
+            photo.set('caption', $('#caption').val());
+
+            var parseFile = new Parse.File(name, file);
+
+            parseFile.save().then(function() {
+                photo.set('photo', parseFile);
+                photo.save(null, {success: function() {
+                    refreshPhotos();
+                }});
+
+            });
+        }
+
+    }
+
+    var initializeForm = function() {
+        var form = $('#upload');
+        form.on('submit', handleUploadSubmit);
+    }
+
+    window.startApp = function() {
+        // Allow user to upload a new photo.
+        initializeForm();
+
+        // Get users recent photos with caption and title
+        refreshPhotos();
+        var query =  new Parse.Query(Photo);
+        // If clicks on a photo, they can draw and save it.
+    }
+
+})()
+
